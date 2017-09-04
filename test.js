28e09dd28c802b5b72fe2012386bf60883d0415d
Luis Guillermo Cuadro Benitez <luis.cuadro@fing.edu.uy>
2017-09-04 11:14:41 -0300

    cambios en el archivo travis.yml

--- test.js
+++ test.js
@@ -6,29 +6,29 @@ web.save('http://localhost:3000/users');
 //obtengo la pagina users
 
 describe('Usuarios', function () {  
-  it('Codigo de estado debe ser 200 y sin errores', function () {
+  it('Código de estado debe ser 200 y sin errores', function () {
     expect(this.err).to.equal(null);
     //compruebo si hay error
     expect(this.res.statusCode).to.equal(200);
     //compruebo codigo de estado
   })
-  it('debe exitir ga@pis.com"', function () {
+  it('debe existir ga@pis.com"', function () {
     expect(this.body).to.include('"email": "ga@pis.com"');
   })
-  it('debe exitir mi@pis.com"', function () {
+  it('debe existir mi@pis.com"', function () {
     expect(this.body).to.include('"email": "mi@pis.com"');
   })
-  it('debe exitir bg@pis.com"', function () {
+  it('debe existir bg@pis.com"', function () {
     expect(this.body).to.include('"email": "bg@pis.com"');
   })
-  it('No debe exitir sebas@pis.com"', function () {
+  it('No debe existir sebas@pis.com"', function () {
     expect(this.body).to.not.include('"email": "sebas@pis.com"');
   });
 });
-describe('pagina', function () {  
+describe('página', function () {  
   web.save('http://localhost:3000/user');
   //obtengo la pagina user
-  it('Codigo de estado debe ser 404 y sin errores', function () {
+  it('Código de estado debe ser 404 y sin errores', function () {
     expect(this.err).to.equal(null);
     //compruebo si hay error
     expect(this.res.statusCode).to.equal(404);
